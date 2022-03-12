9e46b075144e93398f71a0ad2774eded4a621f07
ughurba <ughurba@edu.code.az>
2022-03-12 22:23:45 +0300

    feat:add task-method

---
+++ metod/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace metod
+{
+    internal class Program
+    {
+
+        static void PrintLine(string symbol, int SymbolsCount)
+        {
+            for (int i = 0; i < SymbolsCount; i++)
+            {
+                Console.Write(symbol);
+            }
+
+
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("введите количество символов:");
+            int SymbolsCount = Convert.ToInt32(Console.ReadLine());
+            Console.Write("введите символ:");
+            string symbol = Console.ReadLine();
+            PrintLine(symbol, SymbolsCount);
+
+        }
+    }
+}
